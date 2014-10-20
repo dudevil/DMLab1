9143a929a19465b3ecafc4abbb6afda49d00a10d
Sergey HPC <ovcharenko.ser@gmail.com>
2014-10-20 23:33:48 +0400

    Reformat code. Add parse_other functions for files, other than reports.xml.

--- R code/mozilla_parse.R
+++ R code/mozilla_parse.R
@@ -1,14 +1,30 @@
-reportsxml <- xmlParse("mozilla/Firefox/reports.xml")
-reportsdf <- xmlToDataFrame(reportsxml, stringsAsFactors = F)
-ids <- xpathSApply(reportsxml, "//reports/report[@id]", xmlAttrs)
-reportsdf$id <- ids
+require('XML')
+require('data.table')
 
-resolution_xml <- xmlParse(resolutionURL)
-idNodes <- getNodeSet(resolution_xml, "//report[@id]")
-ids <- lapply(idNodes, function(x) xmlAttrs(x)['id'])
+parse_reports <- function(filename='raw data/reports.xml'){
+  reportsdf <- xmlToDataFrame(xmlParse(filename), stringsAsFactors = F)
+  ids <- xpathSApply(reportsxml, "//reports/report[@id]", xmlAttrs)
+  reportsdf$id <- as.numeric(ids)
+  reportsdf$reporter <- as.numeric(reportsdf$reporter)
+}
 
-val_when <- lapply(idNodes, xpathApply, path = './update/when', xmlValue)
-val_what <- lapply(idNodes, xpathApply, path = './update/what', xmlValue)
+parse_other <-function(filename='raw data/resolution.xml'){
+  xml <- xmlParse(filename)
+  parse_report <- function(xml){
+    xdf <- xmlToDataFrame(xml, stringsAsFactors = F)
+    xdf$id <- xmlAttrs(xml)
+    return(xdf)
+  }
+  rbindlist(xpathApply(xml, '//*/report',parse_report), use.names=T)
+}
 
-resolutions <- data.frame(do.call(rbind.data.frame, mapply(cbind, ids, val_when, val_what)), stringsAsFactors = F)
-names(resolutions) <- c("report id", "update_when", "update_what")
+parse_class <- function(filename='raw data/resolution.xml'){
+  resolution_xml <- xmlParse(filename)
+  idNodes <- getNodeSet(resolution_xml, "//report[@id]")
+  ids <- lapply(idNodes, function(x) xmlAttrs(x)['id'])
+  val_when <- lapply(idNodes, xpathApply, path = './update/when', xmlValue)
+  val_what <- lapply(idNodes, xpathApply, path = './update/what', xmlValue)
+  resolutions <- data.frame(do.call(rbind.data.frame, mapply(cbind, ids, val_when, val_what)), stringsAsFactors = F)
+  names(resolutions) <- c("report id", "update_when", "update_what")
+  return(resolutions)
+}
