e1e7e86788bc2ac1d6786da20ced2bd2fa56bf92
Sergey Ovcharenko <ovcharenko.ser@gmail.com>
2014-10-21 18:03:33 +0400

    Add functions to parse and merge xml files into a data.table.

--- R code/mozilla_parse.R
+++ R code/mozilla_parse.R
@@ -2,22 +2,84 @@ require('XML')
 require('data.table')
 
 parse_reports <- function(filename='raw data/reports.xml'){
-  reportsdf <- xmlToDataFrame(xmlParse(filename), stringsAsFactors = F)
+  # Parses the reports.xml file 
+  #
+  # Args:
+  #   filename: path to the xml file
+  #
+  # Returns:
+  #   A dataframe containing report id, reporter, and opening time
+  
+  reportsxml <- xmlParse(filename)
+  reportsdf <- xmlToDataFrame(reportsxml, stringsAsFactors = F)
+  # get report id's from xml attributes
   ids <- xpathSApply(reportsxml, "//reports/report[@id]", xmlAttrs)
-  reportsdf$id <- as.numeric(ids)
-  reportsdf$reporter <- as.numeric(reportsdf$reporter)
+  # cast data to integers
+  reportsdf$id <- as.integer(ids)
+  reportsdf$reporter <- as.integer(reportsdf$reporter)
+  reportsdf$opening_time <- as.integer(reportsdf$opening_time)
+  return(reportsdf)
 }
 
-parse_other <-function(filename='raw data/resolution.xml'){
+parse_other <-function(filename='raw data/resolution.xml', data_names=c()){
+  # Parses the xml file into the data.table
+  #
+  # Args:
+  #   filename: path to the xml file
+  #   data_names: names for the collumns in the resulting data.table
+  #
+  # Returns:
+  #   A data.table
   xml <- xmlParse(filename)
+  # parse the 'report' xml element, return a dataframe 
   parse_report <- function(xml){
     xdf <- xmlToDataFrame(xml, stringsAsFactors = F)
     xdf$id <- xmlAttrs(xml)
     return(xdf)
   }
-  rbindlist(xpathApply(xml, '//*/report',parse_report), use.names=T)
+  # merge the dataframes from all 'report' elemets into a data.table
+  res = rbindlist(xpathApply(xml, '//*/report',parse_report), use.names=T)
+  # if data_names was given rename collumns
+  if(length(data_names)){
+      names(res)[names(res)!='id'] <- data_names
+  }
+  return(res)
 }
 
+merge_files <- function(files, data_names){
+  # A wrapper around parse_reports and parse_other for multiple files
+  #
+  # Args:
+  #   files: a list of vector of filenames to parse
+  #   data_names: a list of names for corresponding files
+  #
+  # Returns:
+  #   A merged data.table for all files
+  reports <- parse_reports()
+  # parse files
+  frames <- mapply(parse_other, files, data_names, SIMPLIFY = F)
+  # merge results
+  return(Reduce(
+    function(x, y) merge(x,y,by='id', allow.cartesian=TRUE), 
+    append(list(reports), frames)))
+}
+
+build_data <-function(){
+  # An example, parses and merges a number of files into one data.table
+  # and tidies the resulting data
+  df <- merge_files(
+    c('raw data/resolution.xml', 'raw data/op_sys.xml'), 
+    list(c('resolved_when', 'resolved_status'), 
+         c('os_when', 'os')))
+  df$id <- as.integer(df$id)
+  df$os <- as.factor(df$os)
+  df$resolved_status <- as.factor(df$resolved_status)
+  df$os_when <- as.integer(df$os_when)
+  df$resolved_when <- as.integer(df$resolved_when)
+  return(df)
+}
+
+###what was done in class
 parse_class <- function(filename='raw data/resolution.xml'){
   resolution_xml <- xmlParse(filename)
   idNodes <- getNodeSet(resolution_xml, "//report[@id]")
